3214593a26377e270b8cfa7ded76e9752a3295ef
hemanthkt <hmkt1503@gmail.com>
2025-04-29 11:39:01 +0530

    Authentication with google provider and mongodb connection

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import Image from "next/image";
 import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
 import { NextAuthProvider } from "./Providers";
+import TestContentPost from "./components/TestComponent";
 
 export default function Home() {
   return (
     <main>
+      <TestContentPost />
       <div className="absolute z-10 w-full">
         <Navbar />
       </div>
